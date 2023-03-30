a55cc309895dbaf20a26baf59dd38e615a11901b
shobhiit <shobhit.pandey@chicmic.co.in>
2023-03-30 19:35:40 +0530

    added seeder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,16 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+ 
 
+  
+  
+
+
+
+
+
+    
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -64,13 +73,19 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  
+  
+  
+  
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: ENV['PORT'] }
   config.action_mailer.perform_deliveries = true
 
+  config.host =  ENV['DATABASE_HOST'] 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :user_name => '17cfed7cd7d69b',
