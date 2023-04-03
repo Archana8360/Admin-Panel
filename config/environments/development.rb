35cd5ce4a712ff05a67ee589b865ccb38907cbff
Satyendra6396 <satyendra.kumar@chicmic.co.in>
2023-04-03 17:16:48 +0530

    code updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   
   
   
-  
+  config.hosts =  ENV['HOSTS'] 
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
